80557922e489a20ad44a4a45bc606b4cafb7ff08
Dharen <dharenrojantagalicudgithub@gmail.com>
2024-12-28 14:16:19 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CoffeeHaven from './CoffeeHaven';
+
+const App = () => {
+    return (
+        <div>
+            <CoffeeHaven />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/CoffeeHaven.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CoffeeHaven = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1 style={{ fontFamily: 'Arial, sans-serif', color: '#6B4226' }}>
+                Welcome to Coffee Haven
+            </h1>
+        </div>
+    );
+};
+
+export default CoffeeHaven;
