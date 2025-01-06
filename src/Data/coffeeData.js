2b90e93a0131f6761eabf6678046ca62644a2800
Dharen <dharenrojantagalicudgithub@gmail.com>
2025-01-06 09:06:58 +0800

    corrected the image paths

--- src/Data/coffeeData.js
+++ src/Data/coffeeData.js
@@ -4,7 +4,7 @@ const coffeeData = [
     name: "Affogato",
     description:
       "Creamy vanilla gelato topped with hot espresso, a delightful Italian treat.",
-    image: "/Images/Menu/Coffee/Affogato.jpg",
+    image: "./Images/Menu/Coffee/Affogato.jpg",
   },
   {
     id: 1002,
@@ -18,74 +18,74 @@ const coffeeData = [
     name: "Black Tea",
     description:
       "Robust, fully oxidized tea with a rich, earthy, and aromatic flavor.",
-    image: "/Images/Menu/Coffee/Black-Tea.jpg",
+    image: "./Images/Menu/Coffee/Black-Tea.jpg",
   },
   {
     id: 1004,
     name: "Cappuccino",
     description:
       "Espresso with equal parts steamed milk and foam, rich and velvety.",
-    image: "../Images/Menu/Coffee/Cappuccino.jpg",
+    image: "./Images/Menu/Coffee/Cappuccino.jpg",
   },
   {
     id: 1005,
     name: "Coffee Latte Art",
     description: "Intricate designs made in steamed milk atop a creamy latte.",
-    image: "/Images/Menu/Coffee/Coffee-Latte-Art.jpg",
+    image: "./Images/Menu/Coffee/Coffee-Latte-Art.jpg",
   },
   {
     id: 1006,
     name: "Cortado Latte",
     description:
       "Espresso cut with steamed milk for a balanced, silky texture.",
-    image: "public/Images/Menu/Coffee/Cortado-Latte.jpg",
+    image: "./Images/Menu/Coffee/Cortado-Latte.jpg",
   },
   {
     id: 1007,
     name: "Espresso Macchiato",
     description: "Bold espresso topped with a dollop of velvety milk foam",
-    image: "/Images/Menu/Coffee/Espresso-Macchiato.jpg",
+    image: "./Images/Menu/Coffee/Espresso-Macchiato.jpg",
   },
   {
     id: 1008,
     name: "Espresso",
     description:
       "Concentrated coffee shot with a robust, intense, and aromatic profile.",
-    image: "/Images/Menu/Coffee/Espresso.jpg",
+    image: "./Images/Menu/Coffee/Espresso.jpg",
   },
   {
     id: 1009,
     name: "Gin Cinnamon Latte",
     description:
       "Spiced latte with gin, cinnamon, and creamy milk, uniquely indulgent.",
-    image: "/Images/Menu/Coffee/Gin-Cinnamon-Latte.jpg",
+    image: "./Images/Menu/Coffee/Gin-Cinnamon-Latte.jpg",
   },
   {
     id: 1010,
     name: "Hot Chocolate",
     description:
       "Rich, creamy drink made from melted chocolate and steamed milk.",
-    image: "/Images/Menu/Coffee/Hot-Chocolate.jpg",
+    image: "./Images/Menu/Coffee/Hot-Chocolate.jpg",
   },
   {
     id: 1011,
     name: "Matcha Latte",
     description: "Smooth blend of matcha green tea and frothy, creamy milk.",
-    image: "/Images/Menu/Coffee/Matcha-Latte.jpg",
+    image: "./Images/Menu/Coffee/Matcha-Latte.jpg",
   },
   {
     id: 1012,
     name: "Mocha",
     description:
       "Espresso with chocolate syrup, steamed milk, and a hint of sweetness.",
-    image: "/Images/Menu/Coffee/Mocha.jpg",
+    image: "./Images/Menu/Coffee/Mocha.jpg",
   },
   {
     id: 1013,
     name: "Turkish Coffee",
     description:
       "Rich, strong coffee brewed with finely ground beans, served unfiltered.",
-    image: "/Images/Menu/Coffee/Turkish-Coffee.jpg",
+    image: "./Images/Menu/Coffee/Turkish-Coffee.jpg",
   },
 ];
 
