60ced15d130d16ec5430d88c4d7567023a174821
Dharen <dharenrojantagalicudgithub@gmail.com>
2025-01-19 15:04:03 +0800

    final boss

--- src/Components/MainPages/Home.jsx
+++ src/Components/MainPages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import MainContent from "../../Components/HomePages/MainContent";
+// import MainContent from "../../Components/HomePages/MainContent";
+import Opening from "../../Components/HomePages/Opening";
 import AboutInfo from "../../Components/HomePages/AboutInfo";
 import BestSeller from "../../Components/HomePages/BestSeller";
 
 const Home = () => {
   return (
     <div>
+      <Opening />
       {/* <MainContent /> */}
       <AboutInfo />
       <BestSeller />
